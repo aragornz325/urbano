766bb43234c556f5a9283a13fce7f828ec0059fb
chivo <rodrigo.m.quintero@gmail.com>
2024-10-29 02:23:57 -0300

    :ambulance: reparacion del sistemas, se intento instalar I18, para eso se necesitaba instalar dependencias y se rompio todo, no se instala la traduccion

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,4 @@
 import './styles/index.css';
-import './i18n';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
--- frontend/src/models/Favorite/Favorite.ts
+++ frontend/src/models/Favorite/Favorite.ts
@@ -1,4 +1,4 @@
-import Course from '../course/Course';
+import { Course } from '../course/Course';
 import User from '../user/User';
 
 export interface IFavorite {
