e0ebbe9cd7690ea6a03bf6d4f640a6ca30e40732
Chivo <rodrigo.m.quintero@gmail.com>
2024-10-31 11:11:26 -0300

    correccion

--- backend/src/auth/guards/roles.guard.ts
+++ backend/src/auth/guards/roles.guard.ts
@@ -8,29 +8,27 @@ import { Role } from '../../enums/role.enum';
 export class RolesGuard implements CanActivate {
   constructor(private reflector: Reflector) {}
 
-  /* Checks if the user's role is included in the required roles for this route */
 
   canActivate(context: ExecutionContext): boolean {
-    // Retrieve the required roles from the metadata
+
     const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
       context.getHandler(),
       context.getClass(),
     ]);
 
-    // If no specific roles are required, grant access
     if (!requiredRoles) {
       return true;
     }
 
-    // Extract the user object from the request
+
     const { user } = context.switchToHttp().getRequest();
 
-    // Ensure the user and user.role exist before checking roles
+
     if (!user || !user.role) {
       throw new UnauthorizedException('User role not found');
     }
 
-    // Check if the user's role is one of the required roles
+
     return requiredRoles.includes(user.role);
   }
 }
